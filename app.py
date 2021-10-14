18bc4140d8a4119ccf1c63e96b88dc21458add22
Paula Gallucci <webhelp-pgallucci@premium-cloud-support.com>
2021-10-14 12:03:09 +0000

    upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from google.cloud import datastore  
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 datastore_client = datastore.Client()
@@ -10,9 +10,7 @@ kind = 'hello_world'
 def hello_world():
     key = datastore_client.key(kind,'the_data')
     task = datastore_client.get(key)
-    print(task)
-    return "Hello {}!".format(task)
-
+    return  jsonify(task)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
