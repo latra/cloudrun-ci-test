969d98219e85da1eaf0c182ded26b50ca688236e
Paula Gallucci <webhelp-pgallucci@premium-cloud-support.com>
2021-10-14 10:57:40 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from google.cloud import datastore  
+from flask import Flask
+
+app = Flask(__name__)
+datastore_client = datastore.Client()
+kind = 'hello_world'
+
+@app.route("/")
+def hello_world():
+    key = datastore_client.key(kind,'the_data')
+    task = datastore_client.get(key)
+    print(task)
+    return "Hello {}!".format(task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
